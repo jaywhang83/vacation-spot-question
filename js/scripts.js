80caad4c657f4571c6e34b934fa70a01efc00dd6
Jay Whang <jaywhang83@gmail.com>
2016-01-14 21:57:48 -0800

    frame work

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  /*
+  $("#testsubmit").click(function(event){
+    //var answer = $("input#enterHere").val();
+      //$(".here").text(answer);
+      $(".test").show();
+      event.preventDefault();
+  });
+  */
+
+  $(".maybehere").click(function(event){
+    debugger;
+    alert("here");
+    event.preventDefault();
+  });
+});
