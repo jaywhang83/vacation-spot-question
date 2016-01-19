df3a4fa1c7e16cc94a8a402b15292b2dd65e1fb4
Jay Whang <jaywhang83@gmail.com>
2016-01-18 21:31:28 -0800

    add function half of the branch fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,111 @@
+var warOrColdQuestion = "warm or cold?";
+var dryOrHumidQuestion = "dry or humid?";
+var beachOrMountainQuestion = "beach or mountain?";
+var indoorOrOutdoorQuestion = "Do you enjoy outdoor activity or indoor activity?";
+var bigcityOrSmallcityQuestion =  "Do you like big city or small city?";
+var museumOrBarQuestion = "museum or bar?";
+var coffeOrtea = "coffee or tea?";
+var rainOrSnow = "rain or snow?";
+var beerOrwine = "beer or wine?";
+
+var q;
+var p;
 $(document).ready(function(){
 
   $("form#testsubmit").submit(function(event){
     debugger;
-    /*
-    var question0 = "Would you like to answer some question?"
-    var question1 = "Warm or cold?";
-    var question2 = "dry or humid?"; // warm question
-    var question3 = "Beach or mountain?"; // warm question
-    var question4 = "Rain or snow?"; // cold question
-    var question5 = "Do you enjoy outdoor activity or indoor activity?"
-    var question6 = "Do you like big city or small city?";
-    var question7 = "Museum or bar?"
-    var question8 = "Coffee or beer?"
-    */
-
-    var questionBank = ["Would you like to answer some question?",
-                         "Warm or cold?",
-                         "dry or humid?",
-                         "Beach or mountain?",
-                         "Rain or snow?",
-                         "Do you enjoy outdoor activity or indoor activity?",
-                         "Do you like big city or small city?",
-                         "Museum or bar?",
-                         "Coffee or beer?"
-                        ];
-
     var answer = $("input#enterHere").val();
 
-
- //while (answer !== "no") {
-   if (answer === "warm") {
-     $("span.question").text(questionBank[2]);
-   }
-   else if (answer === "dry") {
-     $("span.question").text(questionBank[6]);
-   }
-   else if (answer === "big") {
-     $("span.question").text(questionBank[5]);
-   }
-   else if (answer === "outdoor") {
-      $("span.question").text(questionBank[3]);
+    if (answer === "warm") {
+      $("span.question").text(dryOrHumidQuestion);
     }
-    else if (answer === "beach") {
-      $("span.here").text("Hawaii");
-      $(".answer").show();
-    }
-    else if (answer === "mountain"){
-      $("span.here").text("Austin");
-      $(".answer").show();
-    };
-
-    if (answer === "cold") {
-     $("span.question").text(questionBank[4]);
-    }
-    else if (answer === "rain" || answer === "snow") {
-      $("span.question").text(questionBank[8]);
-       }
-    else if (answer === "beer") {
-      $("span.question").text(questionBank[7]);
-       }
-    else if (answer === "bar") {
-      $("span.here").text("Dublin");
-      $(".answer").show();
-    }
-    else if (answer === "museum") {
-      $("span.here").text("France");
-      $(".answer").show();
+    else {
+      if (answer === "cold"){
+        $("span.question").text(rainOrSnow);
+      }
+      else {
+        route(answer);
+      }
     }
 
+    function route(answer) {
+      if (answer === "humid" || answer === "dry") {
+        $("span.question").text(bigcityOrSmallcityQuestion);
+      }
+      else if (answer === "rain" || answer === "snow"){
+        $("span.question").text(beerOrwine);
+        p = 1;
+      }
+      else {
+        if (p === 1) {
+          smallRoute(answer);
+        }
+      }
+
+      if (answer === "big" || answer === "small") {
+        $("span.question").text(museumOrBarQuestion);
+        q = 1;
+      }
+      else {
+        if (q === 1){
+          bigRoute(answer);
+        }
+      }
+    }
+
+    function bigRoute(answer) {
+      if (answer === "bar") {
+        $("span.question").text(indoorOrOutdoorQuestion);
+      }
+      else if (answer === "museum") {
+        $("span.question").text(beachOrMountainQuestion);
+      }
+      else {
+        if (answer === "indoor") {
+          $("span.here").text("Rome, Italy");
+          $(".answer").show();
+        }
+        else if (answer === "outdoor") {
+          $("span.here").text("Croatia");
+          $(".answer").show();
+        }
+        else if (answer === "beach") {
+          $("span.here").text("Sydney");
+          $(".answer").show();
+        }
+        else if (answer === "mountain") {
+          $("span.here").text("Austin");
+          $(".answer").show();
+        }
+      }
+    }
+
+    function smallRoute(answer) {
+      if (answer === "beer") {
+        $("span.question").text(indoorOrOutdoorQuestion);
+      }
+      else if (answer === "wine") {
+        $("span.question").text(beachOrMountainQuestion);
+      }
+      else {
+        if (answer === "indoor") {
+          $("span.here").text("Venice, Italy");
+          $(".answer").show();
+        }
+        else if (answer === "outdoor") {
+          $("span.here").text("London");
+          $(".answer").show();
+        }
+        else if (answer === "beach") {
+          $("span.here").text("Hawaii");
+          $(".answer").show();
+        }
+        else if (answer === "mountain") {
+          $("span.here").text("Berlin");
+          $(".answer").show();
+        }
+      }
+    }
     event.preventDefault();
-
   });
 });
